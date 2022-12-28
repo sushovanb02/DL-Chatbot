ccffb9e8c9997a5d117d52db43756ef24afad1f0
sushovanb02 <sushovanb02@gmail.com>
2022-12-28 22:37:41 +0530

    inserting data and building database

--- chatbot_db.py
+++ chatbot_db.py
@@ -9,13 +9,13 @@ connection = s3.connect('{}.db'.format(timeframe))
 cur = connection.cursor()
 
 def create_table():
-    cur.execute("CREATE TABLE IF NOT EXISTS parent_reply(parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)")
+    cur.execute("""CREATE TABLE IF NOT EXISTS parent_reply(parent_id TEXT PRIMARY KEY, comment_id TEXT UNIQUE, parent TEXT, comment TEXT, subreddit TEXT, unix INT, score INT)""")
 
 def format_data(data):
     data = data.replace('\n',' newlinechar ').replace('\r',' newlinechar ').replace('"',"'")
     return data
 
-def find_parent(pid):
+def find_existing_score(pid):
     try:
         sql = "SELECT comment FROM parent_reply WHERE comment_id = '{}' LIMIT 1".format(pid)
         cur.execute(sql)
@@ -25,8 +25,64 @@ def find_parent(pid):
         else:
             return False
     except Exception as e:
-        print("find_parent", e)
         return False
+
+# Making sure comments are of an acceptable length for training, and that the comment wasn't removed or deleted
+def acceptable(data):
+    if len(data.split(' ')) > 50 or len(data) < 1:
+        return False
+    elif len(data) > 1000:
+        return False
+    elif data == '[deleted]' or data =='[removed]':
+        return False
+    else:
+        return True
+
+def find_parent(pid):
+    try:
+        sql = "SELECT score FROM parent_reply WHERE parent_id = '{}' LIMIT 1".format(pid)
+        cur.execute(sql)
+        result = cur.fetchone()
+        if result != None:
+            return result[0]
+        else:
+            return False
+    except Exception as e:
+        return False
+
+def transaction_builder(sql):
+    global sql_transaction
+    sql_transaction.append(sql)
+    if len(sql_transaction) > 1000:
+        cur.execute('BEGIN TRANSACTION')
+        for s in sql_transaction:
+            try:
+                cur.execute(s)
+            except:
+                pass
+        connection.commit()
+        sql_transaction = []
+
+def sql_insert_replace_comment(commentid, parentid, parent, comment, subreddit, time, score):
+    try:
+        sql = """UPDATE parent_reply SET parent_id = ?, comment_id = ?, parent = ?, comment = ?, subreddit = ?, unix = ?, score = ? WHERE parent_id = ?;""".format(parentid, commentid, parent, comment, subreddit, int(time), score, parentid)
+        transaction_builder(sql)
+    except Exception as e:
+        print('s-UPDATE insertion',str(e))
+
+def sql_insert_has_parent(commentid, parentid, parent, comment, subreddit, time, score):
+    try:
+        sql = """INSERT INTO parent_reply (parent_id, comment_id, parent, comment, subreddit, unix, score) VALUES ("{}","{}","{}","{}","{}",{},{});""".format(parentid, commentid, parent, comment, subreddit, int(time), score)
+        transaction_builder(sql)
+    except Exception as e:
+        print('s-PARENT insertion',str(e))
+
+def sql_insert_no_parent(commentid, parentid, comment, subreddit, time, score):
+    try:
+        sql = """INSERT INTO parent_reply (parent_id, comment_id, comment, subreddit, unix, score) VALUES ("{}","{}","{}","{}",{},{});""".format(parentid, commentid, comment, subreddit, int(time), score)
+        transaction_builder(sql)
+    except Exception as e:
+        print('s-NO_PARENT insertion',str(e))
 
 # Reading rows which is of the json format
 if __name__ == "__main__":
@@ -36,7 +92,6 @@ if __name__ == "__main__":
 
     with open(r"D:/CSE/projects/DL-Chatbot/chatbot data/RC_2015-01", buffering=1000) as f:
         for row in f:
-            print(row)
             row_counter += 1
             row = json.loads(row)
             parent_id = row['parent_id']
@@ -45,5 +100,22 @@ if __name__ == "__main__":
             score = row['score']
             comment_id = row['name']
             subreddit = row['subreddit']
-
             parent_data = find_parent(parent_id)
+
+            # If there is an existing comment, and if our score is higher than the existing comment's score, we'd like to replace it
+            if score >= 2:
+                if acceptable(body):
+                    existing_comment_score = find_existing_score(parent_id)
+                    if existing_comment_score:
+                        if str(score) > str(existing_comment_score):
+                            sql_insert_replace_comment(comment_id, parent_id, parent_data, body, subreddit, created_utc, score)
+
+                    else:
+                        if parent_data:
+                            sql_insert_has_parent(comment_id, parent_id, parent_data, body, subreddit, created_utc, score)
+                            paired_rows += 1
+                        else:
+                            sql_insert_no_parent(comment_id, parent_id, body, subreddit, created_utc, score)
+            
+            if row_counter % 100000 == 0:
+                print('Total Rows Read: {}, Paired Rows: {}, Time: {}'.format(row_counter, paired_rows, str(datetime.now())))
